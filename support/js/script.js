96481b1a8b63f0e75d17b59b7ca0e59530edb579
Stas-sklyar <61268997+Stas-sklyar@users.noreply.github.com>
2020-06-03 16:45:48 +0300

    Add files via upload

---
+++ support/js/script.js
@@ -0,0 +1,43 @@
+
+//JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP
+function testWebP(callback) {
+
+var webP = new Image();
+webP.onload = webP.onerror = function () {
+callback(webP.height == 2);
+};
+webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+if (support == true) {
+document.querySelector('body').classList.add('webp');
+}else{
+document.querySelector('body').classList.add('no-webp');
+}
+});
+
+
+// show subMenu
+function showSubMenu() {
+	let subMenuList = document.getElementById("subMenuList");
+	subMenuList.classList.toggle("showMenu");
+}
+
+
+// acardion in section "_support"
+let acc = document.getElementsByClassName("questions__title");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+  });
+}
